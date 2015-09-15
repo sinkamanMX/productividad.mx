b7a54d27c9f46a7ffc248e23938b789275202ea0
Enrique Peña Gonzalez <er.penagonzalez@gmail.com>
2015-09-15 10:41:59 -0500

    Update
    Update

--- public/js/atn/jsDashboard.js
+++ public/js/atn/jsDashboard.js
@@ -6,29 +6,29 @@ $( document ).ready(function() {
         navigate: "scroll",
         maxScale: "months",
         minScale: "hours",
-        scale:    "months",
+        scale:    "hours",
         itemsPerPage: 20,
-        /*scrollToToday: true,*/
+        scrollToToday: true,
         onItemClick: function(data) {    
             showDetail(data);
-            console.log(data);
         },
         onAddClick: function(dt, rowId) {
             /*alert("Empty space clicked - add an item!");*/
         },
         onRender: function() {
-            if (window.console && typeof console.log === "function") {
+            /*if (window.console && typeof console.log === "function") {
                 console.log("chart rendered");
-            }
+            }*/
         }
     });
 
+    /*
     $(".gantt").popover({
         selector: ".bar",
         title: "I'm a popover",
         content: "And I'm the content of said popover.",
         trigger: "hover"
-    });
+    });*/
 
     $('#iFrameDetCita').on('load', function () {        
         $('#loader').hide();
